6fd8029cad55df668264b4443941a0044e423ba6
scriptslay3r-code <jackrabbitaltima@gmail.com>
2022-06-07 00:44:20 -0500

    added last sale price

--- atomic-request.py
+++ atomic-request.py
@@ -38,14 +38,33 @@ def get_series():
 	xData = json.loads(dump)
 	series = (xData['data'][0]['data']['series'])
 	return series
+	
+def get_sales():
+	base_url = 'https://wax.api.atomicassets.io/atomicmarket/v1/assets/'
+
+	asset_id = '1099790610714'
+
+	paramaters = '/sales?page=1&limit=100&order=desc'
+
+	url = base_url + asset_id + paramaters
+
+	r = requests.get(url)
+	jData = r.json()
+	dumps = json.dumps(jData)
+	xData = json.loads(dumps)
+	sales = (xData['data'][0]['price'])
+	return sales
 
 
 id = get_id()
 name = get_name()
 multiplier = get_multiplier()
 series = get_series()
+sales = get_sales()
+
 pprint.pprint("assest id: " + id)
 pprint.pprint("name: " + name)
 pprint.pprint("multiplier: " + multiplier)
 pprint.pprint("This beautiful NFT is from the " + series + " series!")
+pprint.pprint("sales history: " + sales)
 
